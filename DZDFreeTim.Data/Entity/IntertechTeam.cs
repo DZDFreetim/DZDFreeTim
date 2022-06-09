78794c8239e3537cb99a023f6c38fc2e178af4bd
esinbelen <81701761+esinbelen@users.noreply.github.com>
2022-06-09 17:18:40 +0300

    yeni entityler oluşturuldu

---
+++ DZDFreeTim.Data/Entity/IntertechTeam.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DZDFreeTim.Data.Entity
+{
+    public class IntertechTeam
+    {
+        [Key]
+        public int IntertechTeamId { get; set; }
+
+        [Required]
+        [Column(TypeName = "nvharchar(50)")]
+        public string TeamName { get; set; }
+
+    }
+}
