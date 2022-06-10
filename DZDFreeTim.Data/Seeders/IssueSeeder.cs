cbf5961ad753cb4a63f672394f9a48c044002a9d
esinbelen <81701761+esinbelen@users.noreply.github.com>
2022-06-10 11:55:06 +0300

    IssueSeeder düzeltildi

--- DZDFreeTim.Data/Seeders/IssueSeeder.cs
+++ DZDFreeTim.Data/Seeders/IssueSeeder.cs
@@ -14,72 +14,70 @@ namespace DZDFreeTim.Data.Seeders
         {
             var issues = new List<Issue>()
             {
-                new IssueStatus()
+                new Issue()
                 {
-                    IssueId="1560510",
+                    IssueId=1560510,
                     IssueName="ITSM-453728",
-                    IssueType="Incident",
-                    CompanyId="Odeabank",
+                    IssueType=1,
+                    CompanyId=2,
                     Summary="internet ve mobil şube parola değiştirme sorunu ",
-                    AssigneeId="batuhan@dzdtech.com",
-                    ReporterId="Odeabank_Jira",
-                    Resolution="Done",
-                    Priority="Low",
+                    AssigneeId=1,
+                    ReporterId=2,
+                    Resolution=3,
+                    Priority=1,
                     Created="17.06.2020 12:25",
                     Resolved="18.06.2020 17:05",
-                    Status="Closed"
+                    Status=2
                 },
-                new IssueStatus()
+                new Issue()
                 {
-                    IssueId="1616434",
+                    IssueId=1616434,
                     IssueName="ITSM-460952",
-                    IssueType="Service Request",
-                    CompanyId="HSBC",
+                    IssueType=2,
+                    CompanyId=3,
                     Summary="İb ve mobil kanallarında son beş login işlemi",
-                    AssigneeId="batuhan@dzdtech.com",
-                    ReporterId="bertanpiset@hsbc.com.tr",
-                    Resolution="Done",
-                    Priority="Low",
+                    AssigneeId=2,
+                    ReporterId=1,
+                    Resolution=2,
+                    Priority=2,
                     Created="30.06.2020 09:06",
                     Resolved="24.07.2020 10:42",
-                    Status="Closed"
+                    Status=2
                 },
-                new IssueStatus()
+                new Issue()
                 {
-                    IssueId="2043290",
+                    IssueId=2043290,
                     IssueName="ITSM-596790",
-                    IssueType="Incident",
-                    CompanyId="Odeabank",
+                    IssueType=1,
+                    CompanyId=2,
                     Summary="İnternet PC kanalında EFT işlemini tamamlayamama sorunu",
-                    AssigneeId="emrepinar@dzdtech.com",
-                    ReporterId="Odeabank_Jira",
-                    Resolution="Done",
-                    Priority="Low",
+                    AssigneeId=3,
+                    ReporterId=2,
+                    Resolution=1,
+                    Priority=1,
                     Created="27.01.2021 15:21",
                     Resolved="24.02.2021 10:41",
-                    Status="Closed"
+                    Status=2
                 },
-                new IssueStatus()
+                new Issue()
                 {
-                    IssueId="2051773",
+                    IssueId=2051773,
                     IssueName="ITSM-599737",
-                    IssueType="Incident",
-                    CompanyId="HSBC",
+                    IssueType=1,
+                    CompanyId=1,
                     Summary="IN54272873 / Onaylanan işlem detayları sayfası",
-                    AssigneeId="damla.sevinc@dzdtech.com",
-                    ReporterId="erdogansengezer@hsbc.com.tr",
-                    Resolution="Done",
-                    Priority="Low",
+                    AssigneeId=1,
+                    ReporterId=1,
+                    Resolution=1,
+                    Priority=3,
                     Created="1.02.2021 15:19",
                     Resolved="24.02.2021 09:55",
-                    Status="Closed"
+                    Status=3
                 },
 
 
             };
-            modelBuilder.Entity<Issue>().HasData(Issue);
-
-
+            modelBuilder.Entity<Issue>().HasData(issues);
 
         }
     }
